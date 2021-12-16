140e5d664a6a2caef6a8136e1d6e38918a12ba0c
dennisfigueroa <denniskfig@gmail.com>
2021-12-15 19:49:04 -0500

    Update the routes in API.

--- Lateral-Flow-Backend/models/Users.js
+++ Lateral-Flow-Backend/models/Users.js
@@ -1,14 +1,16 @@
 //How Node 'imports' a module/package. 
 const mongoose = require('mongoose'); 
 
-//Going to make schema, schema just means what the user is going to look like
+//Going to make schema, schema just means what the user 
+//(being pushed to DB) is going to look like
 const UserSchema = mongoose.Schema({
-    firstname: String,
-    middlename: String, 
-    lastname: String, 
-    dateofbirth: Date, 
-    gender: String, 
-    email: String, 
-    phone: Number, 
+    firstname: {type: String, required: true},
+    middlename: {type: String, required: true},
+    lastname: {type: String, required: true},
+    dateofbirth: {type: Date, required: true},
+    gender: {type: String, required: true},
+    email: {type: String, required: true},
+    phone: {type: Number, required: true},
 })
 
+module.exports = mongoose.model('AllUsers', UserSchema);
--- Lateral-Flow-Backend/server.js
+++ Lateral-Flow-Backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express(); 
 const mongoose = require('mongoose'); 
 const router = express.Router(); 
+const bodyParser = require('body-parser'); 
+
+//Creating middleware where everytime we hit any requets, we make sure the bodyParser runs. 
+app.use(bodyParser.json()); 
 
 
 //Connect to local mongoDB database
@@ -22,9 +26,6 @@ app.get('/', (req, res) => {
     res.send('This is the landing page.'); 
 }); 
 
-app.post('/', (req, res) => {
-    res.send(req.body)
-})
 
 //How we start listening to this server. 
 app.listen(3001, () => console.log("Server started"))
